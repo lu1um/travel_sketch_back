bf94d4d474efc0f3abb84cd0b0a6b8aed432e954
lu1um <emperor1812@gmail.com>
2022-06-06 15:11:44 +0900

    add articles views

--- articles/urls.py
+++ articles/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name='articles'
 urlpatterns = [
     path('', views.article_list_or_create), # 전체 아티클을 응답하거나, 새로운 데이터를 만드는 요청
-    path('<int:article_pk>', views.article_detail_or_update_or_delete), # 특정 아티클을 조작하는 요청
+    path('<int:article_pk>/', views.article_detail_or_update_or_delete), # 특정 아티클을 조작하는 요청
     path('<int:article_pk>/like/', views.like_article), # 좋아요 누르는 요청
     path('<int:article_pk>/comments/', views.create_comment), # 댓글을 작성하는 요청
     path('<int:article_pk>/comments/<int:comment_pk>/', views.comment_update_or_delete), # 댓글을 수정하는 요청
--- articles/views.py
+++ articles/views.py
@@ -3,33 +3,111 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 
-from .models import Article, Comment
+from .models import Article, Comment, News
+from .serializers import ArticleSerializer, CommentSerializer, NewsSerializer
 
 
-
+@api_view(['GET', 'POST'])
 def article_list_or_create(request):
-    pass
+    
+    if request.method == 'GET':
+        articles = Article.objects.order_by('-pk')
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
+@api_view(['GET', 'PUT'])
 def article_detail_or_update_or_delete(request, article_pk):
-    pass
+    article = get_object_or_404(Article, pk=article_pk)
+
+    if request.method == 'GET':
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        if request.user == article.user:
+            serializer = ArticleSerializer(instance=article, data=request.data)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response(serializer.data)
+
+    elif request.method == 'DELETE':
+        if request.user == article.user:
+            article.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
 
 
+@api_view(['POST'])
 def like_article(request, article_pk):
-    pass
+    article = get_object_or_404(Article, pk=article_pk)
+    user = request.user
+    if article.like_users.filter(pk=user.pk).exists():
+        article.like_users.remove(user)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+    else:
+        article.like_users.add(user)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
 
 
+@api_view(['POST'])
 def create_comment(request, article_pk):
-    pass
+    user = request.user
+    article = get_object_or_404(Article, pk=article_pk)
+    
+    serializer = CommentSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save(article=article, user=user)
+        comments = article.comments.all()
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
+@api_view(['PUT', 'DELETE'])
 def comment_update_or_delete(request, article_pk, comment_pk):
-    pass
+    article = get_object_or_404(Article, pk=article_pk)
+    comment = get_object_or_404(Comment, pk=comment_pk)
+
+    if request.method == 'PUT':
+        if request.user == comment.user:
+            serializer = CommentSerializer(instance=comment, data=request.data)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                comments = article.comments.all()
+                serializer = CommentSerializer(comments, many=True)
+                return Response(serializer.data)
+
+    elif request.method == 'DELETE':
+        if request.user == comment.user:
+            comment.delete()
+            comments = article.comments.all()
+            serializer = CommentSerializer(comments, many=True)
+            return Response(serializer.data)
 
 
+@api_view(['GET'])
 def search(request):
-    pass
+    keywords = request.data.keyword.split()
 
 
+@api_view(['GET', 'POST'])
 def news_list_or_create(request):
-    pass
+    if request.method == 'GET':
+        news = News.objects.order_by('-pk')
+        serializer = NewsSerializer(news, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        items = request.data.items
+        for item in items:
+            serializer = NewsSerializer(data=item)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
