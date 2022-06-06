d9c87fa2a5e861ed2b5924c1b9addffc96538f21
lu1um <emperor1812@gmail.com>
2022-06-06 14:24:15 +0900

    fix_timezone

--- travel_sketch/settings.py
+++ travel_sketch/settings.py
@@ -127,9 +127,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'ko-kr'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/seoul'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
